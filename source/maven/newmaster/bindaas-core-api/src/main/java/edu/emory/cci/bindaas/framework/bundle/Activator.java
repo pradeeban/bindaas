2d1352fd314fa68ab9432f4e4434037b4bc7709d
nadirsaghar <nadirsaghar@emory.edu>
2014-05-06 21:39:32 -0500

    Checking in updated maven projects

---
+++ source/maven/newmaster/bindaas-core-api/src/main/java/edu/emory/cci/bindaas/framework/bundle/Activator.java
@@ -0,0 +1,29 @@
+package edu.emory.cci.bindaas.framework.bundle;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.osgi.framework.BundleActivator;
+import org.osgi.framework.BundleContext;
+
+public class Activator implements BundleActivator {
+
+	private static BundleContext context;
+	private Log log = LogFactory.getLog(getClass());
+	
+	public static BundleContext getContext()
+	{
+		return context;
+	}
+	
+	public void start(BundleContext arg0) throws Exception {
+		log.debug("Starting Bundle [bindaas-core-api]");
+		context = arg0;
+
+	}
+
+	public void stop(BundleContext arg0) throws Exception {
+		context = null;
+
+	}
+
+}
