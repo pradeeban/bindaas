3d205e7ef48c4a771e12079bab8a1dc019a33d40
nadirsaghar <nadirsaghar@emory.edu>
2014-05-30 16:36:37 -0500

    fixed pom

--- source/maven/newmaster/bindaas-commons-cxf-wrapper/src/main/java/edu/emory/cci/bindaas/commons/cxf/bundle/Activator.java
+++ source/maven/newmaster/bindaas-commons-cxf-wrapper/src/main/java/edu/emory/cci/bindaas/commons/cxf/bundle/Activator.java
@@ -20,7 +20,7 @@ public class Activator implements BundleActivator {
 	 */
 	public void start(BundleContext bundleContext) throws Exception {
 		Activator.context = bundleContext;
-		log.trace(String.format("Starting Bundle [%s]", context.getBundle().getSymbolicName()));
+		log.debug(String.format("Starting Bundle [%s]", context.getBundle().getSymbolicName()));
 	}
 
 	/*
